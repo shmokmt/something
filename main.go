ad15c12908051a4086c45d2323a373380b380a98
furusax0621 <fur.sax.smallworld@gmail.com>
2023-11-20 23:14:59 +0900

    rand.Seed() is deprecated

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"math/rand"
-	"time"
 )
 
 func main() {
@@ -43,7 +42,6 @@ func main() {
 		"サムルノリ",
 		"サム・アルトマン",
 	}
-	rand.Seed(time.Now().UnixNano())
 	max := len(somethingList)
 	fmt.Printf("寒すぎて、%sになった\n", somethingList[rand.Intn(max)])
 }
